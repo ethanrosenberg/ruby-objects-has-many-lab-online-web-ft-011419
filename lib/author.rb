43c11676ce05ff4f9a5fa82e8b041b690c2ac042
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-01-31 08:11:33 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -19,7 +19,7 @@ class Author
   def add_post_by_title(title)
     newpost = Post.new(title)
     newpost.author = self
-    newpost.author_name = self
+    newpost.author_name = self.name
     @posts << newpost
     @@postcount += 1
   end
